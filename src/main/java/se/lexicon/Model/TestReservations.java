7ef04adb92a6d094d8ebc3baac2bbfb74ddb4562
AgnesNazie <cheagnesmbo@gmail.com>
2025-04-15 20:01:12 +0200

    Tested model

---
+++ src/main/java/se/lexicon/Model/TestReservations.java
@@ -0,0 +1,19 @@
+package se.lexicon.Model;
+
+public class TestReservations {
+    public static void main(String[] args) {
+        Customer customer1 = new Customer(1, "CustomerName1","0123456789", "ABC 123");
+        ParkingSpot spot1 = new ParkingSpot(1, 101);
+        ParkingSpot spot2 = new ParkingSpot(3, 101);
+        System.out.println("spot1 = " + spot1);
+        System.out.println("spot2 = " + spot2);
+        System.out.println("------------------");
+        Reservation reservation1 = new Reservation(customer1, spot1, 2);
+        spot1.occupy();
+
+        System.out.println("reservation1 = " + reservation1);
+        System.out.println("---------------------");
+        System.out.println("spot1 = " + spot1);
+        System.out.println("spot2 = " + spot2);
+    }
+}
